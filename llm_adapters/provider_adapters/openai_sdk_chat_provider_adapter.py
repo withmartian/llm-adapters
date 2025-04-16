bd01dedbb856a89eac3c247d0cbcd967105bad1e
Luka Samkharadze <31849767+LukeSamkharadze@users.noreply.github.com>
2025-04-16 23:11:12 +0400

    feat: add o4-mini

--- llm_adapters/provider_adapters/openai_sdk_chat_provider_adapter.py
+++ llm_adapters/provider_adapters/openai_sdk_chat_provider_adapter.py
@@ -168,6 +168,22 @@ MODELS: list[Model] = [
         context_length=1047576,
         completion_length=32768,
     ),
+    OpenAIModel(
+        name="o4-mini",
+        cost=Cost(prompt=1.10e-6, completion=4.48e-6),
+        context_length=200000,
+        completion_length=100000,
+        supports_stop=False,
+        can_temperature=False,
+    ),
+    OpenAIModel(
+        name="o4-mini-2025-04-16",
+        cost=Cost(prompt=1.10e-6, completion=4.48e-6),
+        context_length=200000,
+        completion_length=100000,
+        supports_stop=False,
+        can_temperature=False,
+    ),
     # OpenAIModel(
     #     name="o1-pro",
     #     cost=Cost(prompt=150.00e-6, completion=600.00e-6),
