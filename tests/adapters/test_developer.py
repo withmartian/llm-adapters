2437692148f8da2b2fe8afd64f1759fbcc5914e0
Luka Samkharadze <31849767+LukeSamkharadze@users.noreply.github.com>
2025-05-12 16:22:58 -0700

    fix: developer tests

--- tests/adapters/test_developer.py
+++ tests/adapters/test_developer.py
@@ -3,7 +3,7 @@ import pytest
 from llm_adapters.client import AsyncOpenAI
 from tests.utils import (
     TEST_CHAT_MODELS,
-    SIMPLE_CONVERSATION_USER_ONLY,
+    SIMPLE_CONVERSATION_USER_DEVELOPER,
     get_response_content_from_vcr,
 )
 from vcr import VCR
@@ -15,7 +15,7 @@ async_client = AsyncOpenAI()
 @pytest.mark.parametrize("model_path", TEST_CHAT_MODELS, ids=str)
 async def test_async(vcr: VCR, model_path: str) -> None:
     adapter_response = await async_client.chat.completions.create(
-        model=model_path, messages=SIMPLE_CONVERSATION_USER_ONLY
+        model=model_path, messages=SIMPLE_CONVERSATION_USER_DEVELOPER
     )
 
     cassette_response = get_response_content_from_vcr(vcr, model_path)
